6da6e9f830f4d9447b1981f24bd22d1aebe6e9bb
kamihatex <plutoismycousin@gmail.com>
2025-12-28 19:59:36 +0200

    first flow set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useCallback } from 'react';
+import {
+  Background,
+  ReactFlow,
+  ReactFlowProvider,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  useReactFlow,
+  Panel,
+  Controls,
+} from '@xyflow/react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import '@xyflow/react/dist/style.css';
+
+const flowKey = 'example-flow';
+
+const getNodeId = () => `randomnode_${+new Date()}`;
+
+const initialNodes = [
+  { id: '1', data: { label: 'Node 1' }, position: { x: 0, y: -50 } },
+  { id: '2', data: { label: 'Node 2' }, position: { x: 0, y: 50 } },
+];
+
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+const SaveRestore = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [rfInstance, setRfInstance] = useState(null);
+  const { setViewport } = useReactFlow();
+
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
+  const onSave = useCallback(() => {
+    if (rfInstance) {
+      const flow = rfInstance.toObject();
+      localStorage.setItem(flowKey, JSON.stringify(flow));
+    }
+  }, [rfInstance]);
+
+  const onRestore = useCallback(() => {
+    const restoreFlow = async () => {
+      const flow = JSON.parse(localStorage.getItem(flowKey));
+
+      if (flow) {
+        const { x = 0, y = 0, zoom = 1 } = flow.viewport;
+        setNodes(flow.nodes || []);
+        setEdges(flow.edges || []);
+        setViewport({ x, y, zoom });
+      }
+    };
+
+    restoreFlow();
+  }, [setNodes, setViewport]);
+
+  const onAdd = useCallback(() => {
+    const newNode = {
+      id: getNodeId(),
+      data: { label: 'Added node' },
+      position: {
+        x: (Math.random() - 0.5) * 400,
+        y: (Math.random() - 0.5) * 400,
+      },
+    };
+    setNodes((nds) => nds.concat(newNode));
+  }, [setNodes]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div style={{height:700}}>
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      onInit={setRfInstance}
+      fitView
+      fitViewOptions={{ padding: 2 }}
+    >
+      <Background />
+      <Controls />
+      <Panel position="top-right">
+        <button className="xy-theme__button" onClick={onSave}>
+          save
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+        <button className="xy-theme__button" onClick={onRestore}>
+          restore
+        </button>
+        <button className="xy-theme__button" onClick={onAdd}>
+          add node
+        </button>
+      </Panel>
+    </ReactFlow>
+    </div>
+  );
+};
 
-export default App
+
+export function App() {
+  return (<ReactFlowProvider>
+    <SaveRestore />
+  </ReactFlowProvider>);
+}
